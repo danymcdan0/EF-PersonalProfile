b85962fea89734bbbe2901655b47a1fa630e1259
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-07-03 10:46:36 +0100

    Fully implemented project controller ui with api

--- PersonalProfileUI/Controllers/ProjectController.cs
+++ PersonalProfileUI/Controllers/ProjectController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+using PersonalProfileUI.Models;
 using PersonalProfileUI.Models.DTOs;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace PersonalProfileUI.Controllers
 {
@@ -13,29 +18,159 @@ namespace PersonalProfileUI.Controllers
 			this.httpClientFactory = httpClientFactory;
 		}
 
-        [HttpGet]
+		[HttpGet]
 		public async Task<IActionResult> Index()
 		{
 			List<ProjectDTO> response = new List<ProjectDTO>();
 
+			var client = httpClientFactory.CreateClient();
+
+			var httpResponseMessage = await client.GetAsync("https://localhost:44385/api/Project");
+
+			httpResponseMessage.EnsureSuccessStatusCode();
+
+			response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<ProjectDTO>>());
+
+			return View(response);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Add()
+		{
+			var client = httpClientFactory.CreateClient();
+
+			var token = "";
+			HttpContext.Request.Cookies.TryGetValue("token", out token);
+
+
+			if (token != null)
+			{
+				return View();
+			}
+
+			return RedirectToAction("Index", "Auth");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(AddProjectViewModel addProjectViewModel)
+		{
 			try
 			{
-				//Get all regions from Web API
 				var client = httpClientFactory.CreateClient();
 
-				var httpResponseMessage = await client.GetAsync("https://localhost:44385/api/project");
+				var token = "";
+
+				HttpContext.Request.Cookies.TryGetValue("token", out token);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+				var httpRequestMessage = new HttpRequestMessage()
+				{
+					Method = HttpMethod.Post,
+					RequestUri = new Uri("https://localhost:44385/api/Project"),
+					Content = new StringContent(JsonSerializer.Serialize(addProjectViewModel), Encoding.UTF8, "application/json")
+				};
+
+				var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+				httpResponseMessage.EnsureSuccessStatusCode();
+
+				var response = await httpResponseMessage.Content.ReadFromJsonAsync<ProjectDTO>();
+
+				if (response != null)
+				{
+					return RedirectToAction("Index", "Project");
+				}
+
+				return View();
+			}
+			catch (Exception)
+			{
+				return RedirectToAction("Index", "Auth");
+			}
+
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(Guid Id)
+		{
+			var client = httpClientFactory.CreateClient();
+
+			var token = "";
+			HttpContext.Request.Cookies.TryGetValue("token", out token);
+
+			if (token == null)
+			{
+				return RedirectToAction("Index", "Auth");
+			}
+
+			var response = await client.GetFromJsonAsync<ProjectDTO>($"https://localhost:44385/api/Project/{Id.ToString()}");
+
+			if (response != null)
+			{
+				return View(response);
+			}
+
+			return View(null);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(ProjectDTO projectDTO)
+		{
+			try
+			{
+				var client = httpClientFactory.CreateClient();
+				var token = "";
+
+				HttpContext.Request.Cookies.TryGetValue("token", out token);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+				var httpRequestMessage = new HttpRequestMessage()
+				{
+					Method = HttpMethod.Put,
+					RequestUri = new Uri($"https://localhost:44385/api/Project/{projectDTO.Id}"),
+					Content = new StringContent(JsonSerializer.Serialize(projectDTO), Encoding.UTF8, "application/json")
+				};
+
+				var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+				httpResponseMessage.EnsureSuccessStatusCode();
+
+				var response = await httpResponseMessage.Content.ReadFromJsonAsync<ProjectDTO>();
+
+				if (response == null)
+				{
+					return RedirectToAction("Edit", "Project");
+				}
+
+				return RedirectToAction("Index", "Project"); ;
+			}
+			catch (Exception)
+			{
+				return RedirectToAction("Index", "Auth");
+			}
+
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(ProjectDTO projectDTO)
+		{
+			try
+			{
+				var client = httpClientFactory.CreateClient();
+
+				var token = "";
+
+				HttpContext.Request.Cookies.TryGetValue("token", out token);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+				var httpResponseMessage = await client.DeleteAsync($"https://localhost:44385/api/Project/{projectDTO.Id}");
 
 				httpResponseMessage.EnsureSuccessStatusCode();
 
-				response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<ProjectDTO>>());
-
+				return RedirectToAction("Index", "Project");
 			}
-			catch (Exception ex)
+			catch (Exception)
 			{
-				//Log the exception
-				throw;
+				return RedirectToAction("Index", "Auth");
 			}
-			return View(response);
 		}
 	}
 }
